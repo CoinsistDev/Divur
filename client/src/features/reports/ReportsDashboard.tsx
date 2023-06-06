a72a4306f76d863c6fc6646afe0239ebadabd1c4
consist.development@gmail.com <netanell@Divur.2nz3xlcgzevundm3azcwtbseue.ax.internal.cloudapp.net>
2023-06-06 07:38:28 +0000

    Add default start and end dates in ReportsDashboard component

--- client/src/features/reports/ReportsDashboard.tsx
+++ client/src/features/reports/ReportsDashboard.tsx
@@ -8,16 +8,20 @@ import he from 'date-fns/locale/he';
 import { Button } from '@mui/material';
 import { observer } from 'mobx-react-lite';
 
-registerLocale('he', he) 
+registerLocale('he', he);
 
 const DEFAULT_DATE = new Date('10/10/1970').toUTCString();
 const NON_TICKET_MESSAGES_LABELS = ['מוסרים מדיוור', 'הגיעו ליעד', 'נקראו', 'נכשלו', 'נשלחו'];
 
 export default observer(function ReportsDashboard() {
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(yesterday.getDate() - 1);
+
   const { departmentStore } = useStore();
   const { currentDepartment } = departmentStore;
-  const [startDate, setStartDate] = useState<Date | null>(null); // Replace null with Date | null
-  const [endDate, setEndDate] = useState<Date | null>(null); // Replace null with Date | null
+  const [startDate, setStartDate] = useState<Date | null>(yesterday);
+  const [endDate, setEndDate] = useState<Date | null>(today);
   const [isExporting, setIsExporting] = useState(false);
 
   defaults.plugins.legend.display = false;
@@ -133,33 +137,39 @@ export default observer(function ReportsDashboard() {
           </Header>
         </Grid.Row>
         <Grid.Row style={{ display: 'flex', justifyContent: 'center' }}>
-        <Form style={{ 
-    display: 'flex', 
-    flexDirection: 'row', 
-    justifyContent: 'center', 
-    alignItems: 'flex-end', // align items to the bottom of the container
-    gap: '20px',
-    width: '100%', // set a consistent width
-    textAlign: 'right' // align contents to the right
-}}>
-    <div style={{ display: 'flex', flexDirection: 'column', width: '200px' }}>
-        <label style={{ marginBottom: '0.5em' }}>מתאריך:</label>
-        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} dateFormat="dd/MM/yyyy" locale="he" isClearable />
-    </div>
-    <div style={{ display: 'flex', flexDirection: 'column', width: '200px' }}>
-        <label style={{ marginBottom: '0.5em' }}>עד תאריך:</label>
-        <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} dateFormat="dd/MM/yyyy" locale="he" isClearable />
-    </div>
-    <Button onClick={() => filterDate(false)} variant="outlined" color="primary" disabled={!startDate || !endDate} style={{ width: '200px', height:'38px' }}>
-        סינון לפי תאריכים
-    </Button>
-    <Form.Button color="green" sx={{ borderColor: 'green', color: 'green' }} onClick={sendReport} disabled={isExporting || !currentDepartment!.totalNonTicketSent} style={{ width: '200px', height:'38px' }}>
-        <span>שליחת דוח למייל</span>
-        <i className="file excel outline icon" style={{ marginLeft: '0.2em' }}></i>
-    </Form.Button>
-</Form>
-
-
+          <Form
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'flex-end', // align items to the bottom of the container
+              gap: '20px',
+              width: '100%', // set a consistent width
+              textAlign: 'right', // align contents to the right
+            }}
+          >
+            <div style={{ display: 'flex', flexDirection: 'column', width: '200px' }}>
+              <label style={{ marginBottom: '0.5em' }}>מתאריך:</label>
+              <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} dateFormat="dd/MM/yyyy" locale="he" isClearable />
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', width: '200px' }}>
+              <label style={{ marginBottom: '0.5em' }}>עד תאריך:</label>
+              <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} dateFormat="dd/MM/yyyy" locale="he" isClearable />
+            </div>
+            <Button onClick={() => filterDate(false)} variant="outlined" color="primary" disabled={!startDate || !endDate} style={{ width: '200px', height: '38px' }}>
+              סינון לפי תאריכים
+            </Button>
+            <Form.Button
+              color="green"
+              sx={{ borderColor: 'green', color: 'green' }}
+              onClick={sendReport}
+              disabled={isExporting || !currentDepartment!.totalNonTicketSent}
+              style={{ width: '200px', height: '38px' }}
+            >
+              <span>שליחת דוח למייל</span>
+              <i className="file excel outline icon" style={{ marginLeft: '0.2em' }}></i>
+            </Form.Button>
+          </Form>
         </Grid.Row>
         <Grid.Row style={{ height: '90%' }}>
           <Grid.Column computer={1}></Grid.Column>
