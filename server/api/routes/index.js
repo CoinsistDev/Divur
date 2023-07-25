33ded18739f90381f975bee935b2f44db01c53d9
consist.development@gmail.com <netanell@Divur.2nz3xlcgzevundm3azcwtbseue.ax.internal.cloudapp.net>
2023-07-25 13:21:47 +0000

    Admin access to bull dashboard

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,24 +1,22 @@
-import { Router } from 'express'
+import { Router } from 'express';
 import { serverAdapter } from '../controllers/message-queue/bull-board.js';
+import { isAdmin, authorization } from '../../middleware/authMiddleware.js';
 
-import blacklistRouter from './blacklist.js'
-import departmentRouter from './department.js'
-import distributionRouter from './distribution.js'
-import messageRouter from './message.js'
-import accountRouter from './account.js'
-import reportRouter from './report.js'
+import blacklistRouter from './blacklist.js';
+import departmentRouter from './department.js';
+import distributionRouter from './distribution.js';
+import messageRouter from './message.js';
+import accountRouter from './account.js';
+import reportRouter from './report.js';
 
+const router = Router();
 
-const router = Router()
+router.use('/blacklist', blacklistRouter);
+router.use('/department', departmentRouter);
+router.use('/distribution', distributionRouter);
+router.use('/message', messageRouter);
+router.use('/account', accountRouter);
+router.use('/report', reportRouter);
+router.use('/admin/queues', authorization, isAdmin, serverAdapter.getRouter());
 
-router.use('/blacklist', blacklistRouter)
-router.use('/department', departmentRouter)
-router.use('/distribution', distributionRouter)
-router.use('/message', messageRouter)
-router.use('/account', accountRouter)
-router.use('/report', reportRouter)
-router.use('/admin/queues', serverAdapter.getRouter());
-
-
-
-export default router
+export default router;
